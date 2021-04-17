c644e48b6a9a5729352d7a098db56d44f4822881
gokayybaz <gokayybaz@192.168.1.2>
2021-04-17 12:58:01 +0300

    second...

--- javaMacOSDeneme/javaMacOSDeneme/JavaMacOSDenemeMain.java
+++ javaMacOSDeneme/javaMacOSDeneme/JavaMacOSDenemeMain.java
@@ -6,6 +6,18 @@ public class JavaMacOSDenemeMain {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Hello Mac OS");
+		
+		
+		String[] user = {"Gökay","Halide","Mustafa"};
+		for (String i : user) {
+			System.out.println(i);
+		}
+		
+		int[] sayiList = {1,5,9,8,5};
+		
+		for (int i : sayiList) {
+			System.out.println(i);
+		}
 
 	}
 
